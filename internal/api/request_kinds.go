5db673f2604ec4ce488520988332b303453fa74d
u2 <u2@lenta.ru>
2022-09-05 14:11:50 +0500

    2 files hbm

--- internal/api/request_kinds.go
+++ internal/api/request_kinds.go
@@ -31,7 +31,6 @@ import (
 func (s *APG) HandleRequestKinds(w http.ResponseWriter, r *http.Request) {
 	gs := models.RequestKind{}
 	ctx := context.Background()
-	out_arr := []models.RequestKind{}
 
 	query := r.URL.Query()
 
@@ -66,6 +65,23 @@ func (s *APG) HandleRequestKinds(w http.ResponseWriter, r *http.Request) {
 		gs1 = string(re.ReplaceAll([]byte(gs1), []byte("''")))
 	}
 
+	gsc := 0
+	err := s.Dbpool.QueryRow(ctx, "SELECT * from func_request_kinds_cnt($1);", gs1).Scan(&gsc)
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	out_arr := make([]models.RequestKind, 0,
+		func() int {
+			if gsc < pgs {
+				return gsc
+			} else {
+				return pgs
+			}
+		}())
+
 	ord := 1
 	ords, ok := query["ordering"]
 	if !ok || len(ords) == 0 {
@@ -97,14 +113,6 @@ func (s *APG) HandleRequestKinds(w http.ResponseWriter, r *http.Request) {
 		}
 
 		out_arr = append(out_arr, gs)
-	}
-
-	gsc := 0
-	err = s.Dbpool.QueryRow(ctx, "SELECT * from func_request_kinds_cnt($1);", gs1).Scan(&gsc)
-
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
 	}
 
 	auth := models.Auth{Create: true, Read: true, Update: true, Delete: true}

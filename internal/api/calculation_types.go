d36ac4329b3fc4c05eec41a1ff11c7820b58fdb9
u2 <u2@lenta.ru>
2022-09-22 12:54:14 +0500

    CalculationType hbm

--- internal/api/calculation_types.go
+++ internal/api/calculation_types.go
@@ -11,9 +11,19 @@ import (
 	"strings"
 
 	"github.com/gorilla/mux"
-	"github.com/jackc/pgx/v4"
+	"github.com/u2lentaru/billbck/internal/adapters/db/pgsql"
 	"github.com/u2lentaru/billbck/internal/models"
+	"github.com/u2lentaru/billbck/internal/services"
+	"github.com/u2lentaru/billbck/internal/utils"
 )
+
+type ifCalculationTypeService interface {
+	GetList(ctx context.Context, pg, pgs int, gs1 string, ord int, dsc bool) (models.CalculationType_count, error)
+	Add(ctx context.Context, ea models.CalculationType) (int, error)
+	Upd(ctx context.Context, eu models.CalculationType) (int, error)
+	Del(ctx context.Context, ed []int) ([]int, error)
+	GetOne(ctx context.Context, i int) (models.CalculationType_count, error)
+}
 
 // HandleCalculationTypes godoc
 // @Summary List calculationtypes
@@ -28,9 +38,11 @@ import (
 // @Success 200 {object} models.CalculationType_count
 // @Failure 500
 // @Router /calculationtypes [get]
-func (s *APG) HandleCalculationTypes(w http.ResponseWriter, r *http.Request) {
-	gs := models.CalculationType{}
+func HandleCalculationTypes(w http.ResponseWriter, r *http.Request) {
+	var gs ifCalculationTypeService
+	gs = services.NewCalculationTypeService(pgsql.CalculationTypeStorage{})
 	ctx := context.Background()
+	auth := utils.GetAuth(r)
 
 	query := r.URL.Query()
 
@@ -65,23 +77,6 @@ func (s *APG) HandleCalculationTypes(w http.ResponseWriter, r *http.Request) {
 		gs1 = string(re.ReplaceAll([]byte(gs1), []byte("''")))
 	}
 
-	gsc := 0
-	err := s.Dbpool.QueryRow(ctx, "SELECT * from func_calculation_types_cnt($1);", gs1).Scan(&gsc)
-
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	out_arr := make([]models.CalculationType, 0,
-		func() int {
-			if gsc < pgs {
-				return gsc
-			} else {
-				return pgs
-			}
-		}())
-
 	ord := 1
 	ords, ok := query["ordering"]
 	if !ok || len(ords) == 0 {
@@ -98,25 +93,14 @@ func (s *APG) HandleCalculationTypes(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	rows, err := s.Dbpool.Query(ctx, "SELECT * from func_calculation_types_get($1,$2,$3,$4,$5);", pg, pgs, gs1, ord, dsc)
+	out_arr, err := gs.GetList(ctx, pg, pgs, gs1, ord, dsc)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	defer rows.Close()
-
-	for rows.Next() {
-		err = rows.Scan(&gs.Id, &gs.CalculationTypeName)
-		if err != nil {
-			log.Println("failed to scan row:", err)
-		}
-
-		out_arr = append(out_arr, gs)
-	}
-
-	auth := models.Auth{Create: true, Read: true, Update: true, Delete: true}
-	out_count, err := json.Marshal(models.CalculationType_count{Values: out_arr, Count: gsc, Auth: auth})
+	out_arr.Auth = auth
+	out_count, err := json.Marshal(out_arr)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
@@ -138,8 +122,12 @@ func (s *APG) HandleCalculationTypes(w http.ResponseWriter, r *http.Request) {
 // @Success 200 {object} models.Json_id
 // @Failure 500
 // @Router /calculationtypes_add [post]
-func (s *APG) HandleAddCalculationType(w http.ResponseWriter, r *http.Request) {
-	a := models.AddCalculationType{}
+func HandleAddCalculationType(w http.ResponseWriter, r *http.Request) {
+	var gs ifCalculationTypeService
+	gs = services.NewCalculationTypeService(pgsql.CalculationTypeStorage{})
+	ctx := context.Background()
+
+	a := models.CalculationType{}
 	body, err := ioutil.ReadAll(r.Body)
 
 	defer r.Body.Close()
@@ -155,11 +143,10 @@ func (s *APG) HandleAddCalculationType(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ai := 0
-	err = s.Dbpool.QueryRow(context.Background(), "SELECT func_calculation_types_add($1);", a.CalculationTypeName).Scan(&ai)
+	ai, err := gs.Add(ctx, a)
 
 	if err != nil {
-		log.Println("Failed execute func_calculation_types_add: ", err)
+		log.Println("Failed execute ifCalculationTypeService.Add: ", err)
 	}
 
 	output, err := json.Marshal(models.Json_id{Id: ai})
@@ -183,7 +170,11 @@ func (s *APG) HandleAddCalculationType(w http.ResponseWriter, r *http.Request) {
 // @Success 200 {object} models.Json_id
 // @Failure 500
 // @Router /calculationtypes_upd [post]
-func (s *APG) HandleUpdCalculationType(w http.ResponseWriter, r *http.Request) {
+func HandleUpdCalculationType(w http.ResponseWriter, r *http.Request) {
+	var gs ifCalculationTypeService
+	gs = services.NewCalculationTypeService(pgsql.CalculationTypeStorage{})
+	ctx := context.Background()
+
 	u := models.CalculationType{}
 	body, err := ioutil.ReadAll(r.Body)
 
@@ -200,11 +191,10 @@ func (s *APG) HandleUpdCalculationType(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ui := 0
-	err = s.Dbpool.QueryRow(context.Background(), "SELECT func_calculation_types_upd($1,$2);", u.Id, u.CalculationTypeName).Scan(&ui)
+	ui, err := gs.Upd(ctx, u)
 
 	if err != nil {
-		log.Println("Failed execute func_calculation_types_upd: ", err)
+		log.Println("Failed execute ifCalculationTypeService.Upd: ", err)
 	}
 
 	output, err := json.Marshal(models.Json_id{Id: ui})
@@ -228,7 +218,11 @@ func (s *APG) HandleUpdCalculationType(w http.ResponseWriter, r *http.Request) {
 // @Success 200 {object} models.Json_ids
 // @Failure 500
 // @Router /calculationtypes_del [post]
-func (s *APG) HandleDelCalculationType(w http.ResponseWriter, r *http.Request) {
+func HandleDelCalculationType(w http.ResponseWriter, r *http.Request) {
+	var gs ifCalculationTypeService
+	gs = services.NewCalculationTypeService(pgsql.CalculationTypeStorage{})
+	ctx := context.Background()
+
 	d := models.Json_ids{}
 	body, err := ioutil.ReadAll(r.Body)
 
@@ -245,15 +239,9 @@ func (s *APG) HandleDelCalculationType(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	res := []int{}
-	i := 0
-	for _, id := range d.Ids {
-		err = s.Dbpool.QueryRow(context.Background(), "SELECT func_calculation_types_del($1);", id).Scan(&i)
-		res = append(res, i)
-
-		if err != nil {
-			log.Println("Failed execute func_calculation_types_del: ", err)
-		}
+	res, err := gs.Del(ctx, d.Ids)
+	if err != nil {
+		log.Println("Failed execute ifCalculationTypeService.Del: ", err)
 	}
 
 	output, err := json.Marshal(models.Json_ids{Ids: res})
@@ -276,23 +264,25 @@ func (s *APG) HandleDelCalculationType(w http.ResponseWriter, r *http.Request) {
 // @Success 200 {object} models.CalculationType_count
 // @Failure 500
 // @Router /calculationtypes/{id} [get]
-func (s *APG) HandleGetCalculationType(w http.ResponseWriter, r *http.Request) {
+func HandleGetCalculationType(w http.ResponseWriter, r *http.Request) {
+	var gs ifCalculationTypeService
+	gs = services.NewCalculationTypeService(pgsql.CalculationTypeStorage{})
+	ctx := context.Background()
+	auth := utils.GetAuth(r)
+
 	vars := mux.Vars(r)
-	i := vars["id"]
-	g := models.CalculationType{}
-	out_arr := []models.CalculationType{}
-
-	err := s.Dbpool.QueryRow(context.Background(), "SELECT * from func_calculation_type_get($1);", i).Scan(&g.Id, &g.CalculationTypeName)
-
-	if err != nil && err != pgx.ErrNoRows {
-		log.Println("Failed execute from func_calculation_type_get: ", err)
+	i, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		i = 0
 	}
 
-	out_arr = append(out_arr, g)
-	auth := models.Auth{Create: true, Read: true, Update: true, Delete: true}
+	out_arr, err := gs.GetOne(ctx, i)
+	if err != nil {
+		log.Println("Failed execute ifCalculationTypeService.GetOne: ", err)
+	}
 
-	// output, err := json.Marshal(g)
-	out_count, err := json.Marshal(models.CalculationType_count{Values: out_arr, Count: 1, Auth: auth})
+	out_arr.Auth = auth
+	out_count, err := json.Marshal(out_arr)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return

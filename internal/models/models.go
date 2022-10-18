a64621ff301b2f6801d21e7e4193d4c3ec9ca677
u2 <u2@lenta.ru>
2022-10-18 14:56:21 +0500

    models.go hbm

--- internal/models/models.go
+++ internal/models/models.go
@@ -80,19 +80,3 @@ type Lang_count struct {
 	Values []Lang `json:"values"`
 	Count  int    `json:"count"`
 }
-
-// //DistributionZone struct
-// type DistributionZone struct {
-// 	Id                   int    `json:"id"`
-// 	DistributionZoneName string `json:"distributionzonename"`
-// }
-// //AddDistributionZone struct
-// type AddDistributionZone struct {
-// 	DistributionZoneName string `json:"distributionzonename"`
-// }
-// //DistributionZone_count  struct
-// type DistributionZone_count struct {
-// 	Values []DistributionZone `json:"values"`
-// 	Count  int                `json:"count"`
-// 	Auth   Auth               `json:"auth"`
-// }
